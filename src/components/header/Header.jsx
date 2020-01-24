8509712273043d2d605fddcc16e571dbff3b96c5
korstan <stas.korda@gmail.com>
2020-01-24 20:34:13 +0500

    used children props in header and navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
+import NavbarLink from './NavbarLink';
 import './Header.css';
 
 export default function Header() {
   return (
     <header>
-      <Navbar />
+      <Navbar>
+        <NavbarLink name="Регистрация" />
+        <NavbarLink name="Войти" />
+      </Navbar>
     </header>
   );
 }
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import NavbarLink from './NavbarLink';
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav>
         <ul className="navbar__links">
-          <NavbarLink name="Регистрация" />
-          <NavbarLink name="Войти" />
+          {this.props.children}
         </ul>
       </nav>
     );
