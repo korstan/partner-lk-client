b6b0b92c905f0d2f0b9c9cb9b43f0b7b5db123cd
korstan <stas.korda@gmail.com>
2020-01-30 19:52:05 +0500

    add profile page boilerplate layout

--- src/components/profile/InfoSection.jsx
+++ src/components/profile/InfoSection.jsx
@@ -5,12 +5,9 @@ export default function InfoSection({ title, children }) {
   return (
       <section className="info-section">
         <h3>{title}</h3>
-        <label>Данные<input type="text"/></label>
-        <label>Данные<input type="text"/></label>
-        <label>Данные<input type="text"/></label>
-        <label>Данные<input type="text"/></label>
-        <label>Данные<input type="text"/></label>
-        <label>Данные<input type="text"/></label>
+        <div className="info-section_data-group">
+          {children}
+        </div>
       </section>
   );
 }
